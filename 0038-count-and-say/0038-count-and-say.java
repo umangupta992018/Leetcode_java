3872564ffdb6aec9cdb844bea36f45afa3743a8b
UMANG GUPTA <83664714+umangupta992018@users.noreply.github.com>
2025-07-01 17:46:35 +0530

    Time: 2 ms (92.19%), Space: 41.4 MB (67.44%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+
+        String say = countAndSay(n-1);
+        StringBuilder res = new StringBuilder();
+        // String res="";
+        int counter = 0;
+        for(int i=0;i<say.length();i++)
+        {
+            counter++;
+            if(i==say.length()-1 || say.charAt(i)!=say.charAt(i+1)){
+                res.append(counter).append(say.charAt(i));//+say.charAt(i);
+                counter=0;
+            }
+        }
+        return res.toString();
+        // String result="";
+        // for(int i=0;i<say.length();i++){
+        //     char ch[]=say.charAt(i);
+
+        //     int count = 0;
+
+        //     while(i<say.length()-1 && say[i] == say[i+1]){
+        //         count++;
+        //         i++;
+        //     }
+
+        //     result += Integer.toString(count) + String.valueOf(ch);
+        // }
+        // return result;
+    }
+}
