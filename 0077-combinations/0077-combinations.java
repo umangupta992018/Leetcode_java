f81d911d32746513f622a479e959e0834bbcdd28
UMANG GUPTA <83664714+umangupta992018@users.noreply.github.com>
2025-07-23 22:33:29 +0530

    Time: 26 ms (15.98%), Space: 94.6 MB (75.69%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> subsets = new ArrayList();
+        subsetgenerate(1,n,new ArrayList(),subsets,k);
+        return subsets;
+    }
+    void subsetgenerate(int start,int n,List<Integer> current,List<List<Integer>> subsets,int k)
+    {
+        if(current.size()==k)
+        subsets.add(new ArrayList(current));
+
+        for(int i=start;i<=n;i++)
+        {
+            current.add(i);
+            subsetgenerate(i+1,n,current,subsets,k);
+            current.remove(current.size()-1);
+        }
+    }
+}
