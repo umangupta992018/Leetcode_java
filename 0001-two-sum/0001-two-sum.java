11df92dbf428e8889c812e676a124028f8427dbb
UMANG GUPTA <83664714+umangupta992018@users.noreply.github.com>
2025-08-26 15:33:33 +0530

    Time: 45 ms (30.79%), Space: 45.4 MB (10.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,7 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
-import java.text.*;
-class Solution 
-{
+class Solution {
     public int[] twoSum(int[] nums, int target) 
     {
-        for(int i=0;i<nums.length-1;i++)
+        for(int i=0;i<nums.length;i++)
         {
             for(int j=i+1;j<nums.length;j++)
             {
@@ -19,24 +14,3 @@ class Solution
         return new int[]{};
     }
 }
-
-// for(int i=0;i<nums.length-1;i++)
-        // {
-        //     for(int j=i+1;j<nums.length;j++)
-        //     {
-        //         if(nums[i]+nums[j]==target)
-        //         {
-        //             return new int[]{i,j};
-        //         }
-        //     }
-        // }
-// for(int i=0;i<nums.length-1;i++)
-// {
-//     for(int j=i+1;j<nums.length;j++)
-//     {
-//         if(nums[i]+nums[j]==target)
-//         {
-//             return new int[]{i,j};
-//         }
-//     }
-// }
